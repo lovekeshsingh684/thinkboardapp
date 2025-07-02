d2fa04428ed4a628fca5761a5e8f346060a9a091
Lovekesh Singh <lovekeshsingh374@gmail.com>
2025-07-02 20:22:49 +0530

    second change in files

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import path, { resolve } from "path"
 
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
-import rateLimiter from './middleware/ratelimiter.js'
+import rateLimiter from './middleware/rateLimiter.js'
 
 
 dotenv.config()
